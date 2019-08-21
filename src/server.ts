cf3d6b11fe8df0091e80f059a23847801bad1f72
Warrian <ianscardoso@hotmail.com>
2019-08-20 22:49:33 -0300

    adding mongoose

--- src/server.ts
+++ src/server.ts
@@ -1,18 +1,29 @@
 import * as restify from 'restify'
+import * as mongoose from 'mongoose'
 
 export class Server {
     application: restify.Server
 
-    Start() {
+    initDatabase() {
+        mongoose.connect('mongodb://localhost:27017/apocalypse')
+    }
+
+    initRoutes() {
+        this.application.get('/', (req, res, next) => {
+            res.send({ message: 'ok' })
+            next()
+        })
+    }
+
+    bootstrap() {
+        this.initDatabase()
+
         this.application = restify.createServer({
             name: 'apocalypse-api',
             version: '1.0.0'
         })
 
-        this.application.get('/', (req, res, next) => {
-            res.send({ message: 'ok' })
-            next()
-        })
+        this.initRoutes()
 
         this.application.listen(8080, () => {
             console.log('listening on 8080')
